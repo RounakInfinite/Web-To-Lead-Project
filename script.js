99d9d7e15fa8b803bf17cf9141e1b580ccf79cac
Rounak Mukherjee <roni26aec@gmail.com>
2025-04-10 14:02:56 +0530

    recaptcha changes added

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-function beforesubmit(){
-    let inputdate = document.querySelector(".inputdate")
-    let outputdate = document.querySelector(".outputdate")
-    console.log("Input Date : ", inputdate.value) // String --> date (en_IN)
-    
-    let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN")
-    outputdate.value = formattedDate
+function beforesubmit() {
+  let inputdate = document.querySelector(".inputdate");
+  let outputdate = document.querySelector(".outputdate");
+  console.log("Input Date : ", inputdate.value); // String --> date (en_IN)
+
+  let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+  outputdate.value = formattedDate;
 }
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
